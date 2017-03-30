5ab95caf744edaa2355118ea77d0b5482039505d
Loic Vigneron <loic.vigneron@gmail.com>
2017-03-30 22:33:14 +0200

    Update config file to avoid conflict with original nerves system

--- nerves.exs
+++ nerves.exs
@@ -5,14 +5,14 @@ version =
   |> File.read!
   |> String.strip
 
-pkg = :nerves_system_rpi3
+pkg = :elicopter_system_rpi3
 
 config pkg, :nerves_env,
   type: :system,
   version: version,
   compiler: :nerves_package,
   artifact_url: [
-    "https://github.com/nerves-project/#{pkg}/releases/download/v#{version}/#{pkg}-v#{version}.tar.gz",
+    "https://github.com/elicopter/#{pkg}/releases/download/v#{version}/#{pkg}-v#{version}.tar.gz",
   ],
   platform: Nerves.System.BR,
   platform_config: [
